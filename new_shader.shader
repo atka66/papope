c7c085b2044ddf60fc56e9fcd755f9cac287bd9c
atka66 <atkavagyok@gmail.com>
2020-10-15 13:38:39 +0200

    proper text color and outline

--- new_shader.shader
+++
@@ -1,6 +0,0 @@
-shader_type canvas_item;
-
-void fragment() {
-	vec2 asd = UV;
-	COLOR = texture(TEXTURE, asd);
-}
--- objects/CustomLabel.gd
+++ objects/CustomLabel.gd
@@ -2,6 +2,8 @@ extends Node2D
 
 export var text = ''
 export var fontSize = 1
+export(bool) var outline = false
+export(Color) var color = Color.white
 export(Array, SpriteFrames) var frames
 
 func set_text(_text):
@@ -9,7 +11,10 @@ func set_text(_text):
 
 func _ready():
 	set_text(text)
+	$Label.add_color_override("font_color", color)
 	$Label.rect_scale = Vector2(fontSize, fontSize)
+	if !outline:
+		$Label.set_theme(preload("res://fonts/NonOutlinedTheme.tres"))
 	if frames.size() > 0:
 		$AnimatedSprite1.set_sprite_frames(frames[0])
 		$AnimatedSprite1.play()
