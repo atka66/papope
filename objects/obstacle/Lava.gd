59cf1507b07b3d559f410a70a46a26fc2dcfa0fd
atka66 <atkavagyok@gmail.com>
2020-11-28 17:39:11 +0100

    particles in lava

--- objects/obstacle/Lava.gd
+++ objects/obstacle/Lava.gd
@@ -1,5 +1,10 @@
 extends Area2D
 
+func _ready():
+	$Particles.process_material = preload("res://resources/LavaPar.tres").duplicate()
+	$Particles.scale = Vector2.ONE / scale
+	$Particles.process_material.emission_box_extents *= Vector3(scale.x, scale.y, 0)
+	$Particles.amount *= scale.x * scale.y
 
 func _on_Lava_body_entered(body):
 	if body.is_in_group('players'):
