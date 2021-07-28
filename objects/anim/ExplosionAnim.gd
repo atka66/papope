6db4204e6a53f1e7ebf3a5ab59e317b2f62b72b7
atka66 <atkavagyok@gmail.com>
2021-07-28 22:29:12 +0200

    crate done

--- objects/anim/ExplosionAnim.gd
+++ objects/anim/ExplosionAnim.gd
@@ -28,7 +28,11 @@ func _ready():
 	for destructible in get_tree().get_nodes_in_group('destructible'):
 		var dist = position.distance_to(destructible.position)
 		if dist < 150:
-			destructible.destroy(position.direction_to(destructible.position) * 100)
+			destructible.destroy(position.direction_to(destructible.position) * 50)
+	for particle in get_tree().get_nodes_in_group('destructionparticle'):
+		var dist = position.distance_to(particle.position)
+		if dist < 150:
+			particle.impulse(position.direction_to(particle.position) * 150)
 	$Audio.stream = Res.AudioExplode[randi() % len(Res.AudioExplode)]
 	$Audio.play()
 
--- objects/obstacle/CrateParticle.gd
+++ objects/obstacle/CrateParticle.gd
@@ -1,4 +1,12 @@
 extends RigidBody2D
 
 func _ready():
-	pass
+	$Sprite.frame = randi() % $Sprite.hframes
+
+func impulse(direction):
+	var actualImpulse = Vector2(
+		(direction.x + (randi() % 100) - 50) * (randi() % 10), 
+		(direction.y + (randi() % 100) - 50) * (randi() % 10)
+	)
+	apply_central_impulse(actualImpulse)
+	add_torque((randi() % 300) - 150)
