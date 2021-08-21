0a3270ceb1a4fd4f88e0f37eff2e175b16280f86
atka66 <atkavagyok@gmail.com>
2021-08-21 15:59:16 +0200

    spawner now spawns particles too

--- objects/system/ObstacleSpawner.gd
+++ objects/system/ObstacleSpawner.gd
@@ -1,13 +1,26 @@
 extends Node2D
 
-func _ready():
-	spawnObstacle()
+export(int) var height = 320
 
-func spawnObstacle():
-	yield(get_tree().create_timer((randf() * 30) + 1), "timeout")
-	var destructible = Res.Destructible.instance()
-	destructible.position = position
-	destructible.sprite = Res.SpriteDestructibleCrate
-	destructible.particleSprite = Res.SpriteDestructibleParticleCrate
-	get_parent().add_child(destructible)
-	var rerun = spawnObstacle()
+func _ready():
+	spawnCrate()
+	spawnCrateParticle()
+
+func spawnCrate():
+	yield(get_tree().create_timer((randf() * 6) + 1), "timeout")
+	var crate = Res.Destructible.instance()
+	crate.position = position
+	crate.position.y += randi() % height
+	crate.sprite = Res.SpriteDestructibleCrate
+	crate.particleSprite = Res.SpriteDestructibleCrate
+	get_parent().add_child(crate)
+	var rerun = spawnCrate()
+
+func spawnCrateParticle():
+	yield(get_tree().create_timer(randf() * 3), "timeout")
+	var cratePar = Res.DestructibleParticle.instance()
+	cratePar.position = position
+	cratePar.position.y += randi() % height
+	cratePar.sprite = Res.SpriteDestructibleParticleCrate
+	get_parent().add_child(cratePar)
+	var rerun = spawnCrateParticle()
