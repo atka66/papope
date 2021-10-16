ce0825953d99887d563149931c5275ca44e620bc
atka66 <atkavagyok@gmail.com>
2021-10-16 18:16:41 +0200

    obstacles spawn more frequently on conveyor

--- objects/system/ObstacleSpawner.gd
+++ objects/system/ObstacleSpawner.gd
@@ -7,7 +7,7 @@ func _ready():
 	spawnCrateParticle()
 
 func spawnCrate():
-	yield(get_tree().create_timer((randf() * 5) + 1), "timeout")
+	yield(get_tree().create_timer((randf() * 2) + 1), "timeout")
 	var crate = Res.Destructible.instance()
 	crate.position = position
 	crate.position.y += randi() % height
@@ -17,7 +17,7 @@ func spawnCrate():
 	var rerun = spawnCrate()
 
 func spawnCrateParticle():
-	yield(get_tree().create_timer(randf() * 2), "timeout")
+	yield(get_tree().create_timer(randf() * 0.5), "timeout")
 	var cratePar = Res.DestructibleParticle.instance()
 	cratePar.position = position
 	cratePar.position.y += randi() % height
