35aa405cb01a3a2954dc8b838fa547208a77406f
atka66 <atkavagyok@gmail.com>
2023-06-18 16:22:21 +0200

    lobby bg

--- global/Global.gd
+++ global/Global.gd
@@ -1,1 +1,146 @@
 extends Node
+
+var MovingBackgroundNode: Node2D
+
+# main debug mode switch (players joined without controllers, debug key to start game, etc)
+const DEBUG = false
+
+const VERSION = '1.3.5'
+const TEAM_COLORS = {
+	0: Color(0.9, 0.2, 0.2),
+	1: Color(0, 0.3, 0.7),
+	2: Color(0.5, 0.9, 0.0),
+	3: Color(1.0, 0.7, 0),
+	4: Color(0.2, 0.2, 0.2)
+}
+const HINT_STRINGS = [
+	["DEATH MAKES YOU DIE"],
+	["BE CAREFUL NOT TO", "FALL OFF THE SHIP"],
+	["TRAPS STUN FOR 2 SECONDS"],
+	["SHIELD LASTS FOR 5 SECONDS"],
+	["STAYING OUT OF BOUNDS FOR", "TOO LONG RESULTS IN DEATH"],
+	["SHIELD DOES NOT SAVE YOU", "FROM FALLING OFF SHIPS", "OR STAYING OUT FOR LONG"],
+	["DYNAMITES TEND TO BOUNCE", "BACK FROM A LOT OF THINGS"],
+	["YOU CAN AVOID REVOLVER", "SHOTS BY HIDING BEHIND", "CERTAIN OBJECTS"],
+	["PICKING UP A POWERUP", "REPLACES YOUR CURRENTLY", "EQUIPPED ONE"],
+	["YOU CANNOT MOVE IN SPACE", "ONLY DASH"],
+	["SAME COLOR MEANS SAME TEAM"]
+]
+enum DeathEnum {
+	DEFAULT, EXPLOSION, REVOLVER, WHIP, WATER, LASER, GHOST, TRAP
+}
+const DEATH_STRINGS = {
+	DeathEnum.DEFAULT : ['oof', 'rip', 'dead', 'oops', 'bye'],
+	DeathEnum.EXPLOSION : ['boom', 'kaboom'],
+	DeathEnum.REVOLVER : ['bang', 'bam'],
+	DeathEnum.WHIP : ['huts', 'crack'],
+	DeathEnum.WATER : ['splash', 'blub'],
+	DeathEnum.LASER : ['zap', 'bzzz'],
+	DeathEnum.GHOST : ['aaah', 'spooked'],
+	DeathEnum.TRAP : ['snap', 'got em']
+}
+enum AchiEnum {
+	UNDERDOG, DEMOLITION_MAN, GUNSLINGER, HUTS_HUTS, DAREDEVIL,
+	GUERRILLA, CARELESS, TRAITOR, NO_REFUNDS, JATSZUNK_MAST, 
+	WAKA_WAKA, GHOSTBUSTER, SPOOKED, TRIPLE_KILL, AINT_GON_FIT, CHICKEN_DINNER
+}
+const ACHIEVEMENTS = {
+	AchiEnum.UNDERDOG : ['underdog', 'low overall score'], # has <= 1/4 of the needed score at the end (score limit min 3)
+	AchiEnum.DEMOLITION_MAN : ['demo man', 'great dynamite damage'], # dynamite dmg / thrown dynamites > 75
+	AchiEnum.GUNSLINGER : ['gunslinger', 'accurate with the revolver'], # revolver accuracy over 75%
+	AchiEnum.HUTS_HUTS : ['huts-huts', 'accurate with the whip'], # whip accuracy over 75%
+	AchiEnum.DAREDEVIL : ['daredevil', 'won a round with low health'], # win a round with <= 10% of health
+	AchiEnum.GUERRILLA : ['guerrilla', 'well placed traps'], # sprung traps (on enemy) / laid traps > 75%
+	AchiEnum.CARELESS : ['careless', 'stepped in his own trap'], # sprung his own trap
+	AchiEnum.TRAITOR : ['traitor', 'killed his own teammate'], # killed his own teammate with revolver, dynamite or whip
+	AchiEnum.NO_REFUNDS : ['no refunds', 'shield on, still died'], # die while having active shield
+	AchiEnum.JATSZUNK_MAST : ['jatszunk mast', 'be underdog with bazsi skin'], # be underdog with bazsi skin (0)
+	AchiEnum.WAKA_WAKA : ['waka waka', 'ate a lot of pellets'], # ate a lot of pellets
+	AchiEnum.GHOSTBUSTER : ['ghostbuster', 'killed a lot of ghosts'], # killed 5 ghosts
+	AchiEnum.SPOOKED : ['spooked', 'scared of ghosts'], # killed by a ghost
+	AchiEnum.TRIPLE_KILL : ['triple kill', 'killed 3 enemies in a round'], # 3 enemies killed in 1 round
+	AchiEnum.AINT_GON_FIT : ['aint gon fit', 'too many achievements!'], # has at least 7 achievements
+	AchiEnum.CHICKEN_DINNER : ['chicken dinner', 'i am a chicken']
+}
+enum PerkEnum {
+	AKIMBO, ARMORED, FAST, SPIKY, CUDDLES, REVERSE,
+	BACKFIRE, SLOW, NO_LEGS, TIME_BOMB,
+	RESET, RIGHT, LONG_ARMS, CHICKEN,
+	HEALTHY, UNHEALTHY, REGENERATION, VAMPIRE, PREPARED
+}
+
+const PERKS : Dictionary = {
+	PerkEnum.AKIMBO: ['akimbo', 'all pickups are doubled', 1],
+	PerkEnum.ARMORED: ['armored', 'incoming damage halved', 2], 
+	PerkEnum.FAST: ['fast', 'doubled movement speed', 3],
+	PerkEnum.SPIKY: ['spiky', 'touching players hurts them', 4],
+	PerkEnum.CUDDLES: ['cuddles', 'touching players heals self', 5],
+	PerkEnum.REVERSE: ['reverse', 'inverted movement', 6],
+	PerkEnum.BACKFIRE: ['backfire', 'inverted aim', 7],
+	PerkEnum.SLOW: ['slow', 'halved movement speed', 8],
+	PerkEnum.NO_LEGS: ['no legs', 'can only dash', 9],
+	PerkEnum.TIME_BOMB: ['time bomb', 'explodes after 1 minute', 10],
+	PerkEnum.RESET: ['reset', 'previous cards removed', 11],
+	PerkEnum.RIGHT: ['right', 'because i cannot look left', 12],
+	PerkEnum.LONG_ARMS: ['long arms', 'longer aim range', 13],
+	PerkEnum.CHICKEN: ['chicken', 'you are a chicken', 14],
+	PerkEnum.HEALTHY: ['healthy', 'doubled max health', 15],
+	PerkEnum.UNHEALTHY: ['unhealthy', 'halved max health', 16],
+	PerkEnum.REGENERATION: ['regeneration', 'regenerates health', 17],
+	PerkEnum.VAMPIRE: ['vampire', 'dealing damage heals', 18],
+	PerkEnum.PREPARED: ['prepared', 'starts with random powerup', 19]
+}
+
+const DAMAGE_LAVA = 1
+const DAMAGE_REVOLVER = 20
+const DAMAGE_WHIP = 30
+const DAMAGE_SPACERAY = 25
+const DAMAGE_CAR = 20
+const DAMAGE_TRAP = 35
+const DAMAGE_CACTUS = 10
+const DAMAGE_SPIKY = 5
+const HEAL_CUDDLES = 5
+const HEAL_REGENERATION = 2
+
+const SKIN_COUNT = 6
+const TEAM_COLOR_STRINGS = {
+	0: "RED",
+	1: "BLUE",
+	2: "GREEN",
+	3: "YELLOW"
+}
+
+const CONVEYOR_VEL_AREA = 0.68
+const CONVEYOR_VEL_RIGID = Vector2(-200.0, 0)
+
+var options = {
+	'mode': ['normal', 'one-hit', 'cards'],
+	'map': ['random', 'hell', 'western', 'ship', 'space', 'highway', 'pacman', 'conveyor'],
+	'rounds': [1, 3, 5, 9]
+}
+var optionsSelected = {
+	'mode': ProjectSettings.get("papope/default_option_mode"),
+	'map': ProjectSettings.get("papope/default_option_map"),
+	'rounds': ProjectSettings.get("papope/default_option_rounds")
+}
+var currentOption = options.keys()[0]
+
+var playersConnected = [false, false, false, false]
+var playersJoined = [false, false, false, false]
+var playersPoints = [0, 0, 0, 0]
+#var playersPoints = [0, 3, 2, 3]
+var playersSkin = [0, 1, 2, 3]
+var playersTeam = [0, 1, 2, 3]
+var playersKills = [0, 0, 0, 0]
+var playersMaxHp = [100, 100, 100, 100]
+
+var playersCrowned = [false, false, false, false]
+var playersFrozen = false
+var playersAchievements = [[], [], [], []]
+#var playersAchievements = [[AchiEnum.UNDERDOG, AchiEnum.JATSZUNK_MAST, AchiEnum.NO_REFUNDS, AchiEnum.GUNSLINGER, AchiEnum.DAREDEVIL, AchiEnum.TRIPLE_KILL, AchiEnum.JATSZUNK_MAST], [AchiEnum.TRIPLE_KILL, AchiEnum.TRIPLE_KILL], [], []]
+enum StatEnum {REV_USE, REV_HIT, DYN_USE, DYN_DMG, WHP_USE, WHP_HIT, TRP_USE, TRP_HIT, PELLETS, GHOST_KILL}
+var playersStats = []
+var playersPerks = [[], [], [], []]
+#var playersPerks = [[PerkEnum.HEALTHY], [PerkEnum.UNHEALTHY], [], []]
+
+var selectedMap = 'none'
--- scenes/Lobby.gd
+++ scenes/Lobby.gd
@@ -3,6 +3,3 @@ extends Node2D
 func _ready():
 	$Anim.play("fade_in")
 	get_node('/root/Music').play('menu')
-
-func _process(delta):
-	pass
