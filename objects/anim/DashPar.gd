91ca12603954448af8f6c82450b8e62bc89914e9
atka66 <atkavagyok@gmail.com>
2023-06-18 10:04:22 +0200

    clean project

--- objects/anim/DashPar.gd
+++
@@ -1,5 +0,0 @@
-extends CPUParticles2D
-
-func _process(delta):
-	if !emitting:
-		queue_free()
--- objects/system/PlayzoneShader.gd
+++
@@ -1,7 +0,0 @@
-tool
-extends Node2D
-
-export(Shader) var shader
-
-func _ready():
-	$ShaderRect.material.shader = shader
