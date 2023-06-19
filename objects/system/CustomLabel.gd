8948f21e119bd6029d36f25f766b32c97a013e09
atka66 <atkavagyok@gmail.com>
2023-06-19 19:10:55 +0200

    customlabel placeholder + lobby menulabels

--- objects/system/CustomLabel.gd
+++ objects/system/CustomLabel.gd
@@ -11,12 +11,28 @@ extends Node2D
 		fontSize = new_fontSize
 		setFontSize(new_fontSize)
 
+enum ALIGNMENTS {LEFT, CENTER, RIGHT}
+@export var alignment: ALIGNMENTS = ALIGNMENTS.LEFT:
+	set(new_alignment):
+		alignment = new_alignment
+		setAlignment(new_alignment)
+
 func _ready():
 #	setText(text)
 	setFontSize(fontSize)
+	setAlignment(alignment)
 
 func setText(new_text):
 	pass
 	
 func setFontSize(new_fontSize):
 	$PlaceholderSprite.scale = Vector2(new_fontSize, new_fontSize)
+
+func setAlignment(new_alignment):
+	match new_alignment:
+		ALIGNMENTS.LEFT:
+			$PlaceholderSprite.offset.x = 0
+		ALIGNMENTS.CENTER:
+			$PlaceholderSprite.offset.x = -18
+		ALIGNMENTS.RIGHT:
+			$PlaceholderSprite.offset.x = -36
