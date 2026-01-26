f515fa18798e6b834df2fc39c2de9fa35e9dbc28
atka66 <atkavagyok@gmail.com>
2026-01-26 20:09:01 +0100

    card perk delta fix

--- objects/system/perk/PerkSlot.gd
+++ objects/system/perk/PerkSlot.gd
@@ -11,21 +11,24 @@ func _ready():
 	$Sprite.hframes = len(Global.PerkEnum) + 1
 	$Sprite.frame = frame
 
-# TODO account delta
-func _process(delta):
-	if falling:
-		velocity.y += 1
-		$Sprite.rotation_degrees += rot
-		$Sprite.position += velocity
-
 func bump() -> void:
 	$Anim.play("bump")
 
-func dispose():
-	velocity = Vector2(randf_range(-4.0, 4.0), randf_range(-13.0, -10.0))
-	rot = 30 - (randi() % 30)
-	falling = true
-	await get_tree().create_timer(1.0).timeout
+func dispose() -> void:
+	var fallTime : float = randf_range(0.5, 0.9)
+
+	var height = randi_range(800, 1000)
+	var ySubtween = create_tween()
+	ySubtween.tween_property($Sprite, "position:y", -height * 0.1, fallTime * 0.3).set_trans(Tween.TRANS_QUAD).set_ease(Tween.EASE_OUT)
+	ySubtween.tween_property($Sprite, "position:y", height * 0.9, fallTime * 0.7).set_trans(Tween.TRANS_CUBIC).set_ease(Tween.EASE_IN)
+
+	var fallTween = create_tween().set_parallel(true)
+	fallTween.tween_subtween(ySubtween)
+	fallTween.tween_property($Sprite, "position:x", randi_range(-50, 50), fallTime)
+	fallTween.tween_property($Sprite, "rotation_degrees", randi_range(-1000, 1000), fallTime)
+	fallTween.chain().tween_callback(explode)
+
+func explode() -> void:
 	var explosionAnim = Res.ExplosionAnimObject.instantiate()
 	explosionAnim.position = global_position + $Sprite.position
 	explosionAnim.harmful = false
